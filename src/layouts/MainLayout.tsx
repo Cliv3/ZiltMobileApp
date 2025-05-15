1691eef65532e3d4ae92f1acd8ffa9d1eff49801
Dmitrii Semenov <dima.cemenov70@yandex.ru>
2025-05-15 11:48:35 -0400

    remove unnecessary

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router-dom';
-import StatusBar from '../components/common/StatusBar';
 
 export default function MainLayout() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <StatusBar />
       
       <main className="flex-1 container max-w-md mx-auto pb-20">
         <Outlet />
